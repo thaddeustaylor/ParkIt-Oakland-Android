01933eeac63dea139ade760cf8d3af452bfac56b
Vince Tran <hello@vincetran.me>
2012-03-25 15:44:12 -0400

    Added some functionality for what we can do as an edit screen

--- src/edu/pitt/designs1635/ParkIt/Information.java
+++ src/edu/pitt/designs1635/ParkIt/Information.java
@@ -13,6 +13,9 @@ import android.widget.TextView;
 public class Information extends Activity
 {
 	private ParkingLocation pl;
+	private TextView nameValue, typeValue, paymentValue, limitValue, rateValue;
+	private EditText nameValueEdit, typeValueEdit, paymentValueEdit, limitValueEdit, rateValueEdit;
+	private Button edit, save;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -24,11 +27,16 @@ public class Information extends Activity
 
 		pl = extras.getParcelable("edu.pitt.designs1635.ParkIt.location");
 
-		TextView nameValue = (TextView) this.findViewById(R.id.namevalue);
-		TextView typeValue = (TextView) this.findViewById(R.id.typevalue);
-		TextView paymentValue = (TextView) this.findViewById(R.id.paytypevalue);
-		TextView limitValue = (TextView) this.findViewById(R.id.limitvalue);
-		TextView rateValue = (TextView) this.findViewById(R.id.ratevalue);
+		nameValue = (TextView) this.findViewById(R.id.namevalue);
+		nameValueEdit = (EditText) findViewById(R.id.namevalue_edit);
+		typeValue = (TextView) this.findViewById(R.id.typevalue);
+		typeValueEdit = (EditText) findViewById(R.id.typevalue_edit);
+		paymentValue = (TextView) this.findViewById(R.id.paytypevalue);
+		paymentValueEdit = (EditText) findViewById(R.id.paytypevalue_edit);
+		limitValue = (TextView) this.findViewById(R.id.limitvalue);
+		limitValueEdit = (EditText) findViewById(R.id.limitvalue_edit);
+		rateValue = (TextView) this.findViewById(R.id.ratevalue);
+		rateValueEdit = (EditText) findViewById(R.id.ratevalue_edit);
 
 		nameValue.setText(pl.getName());
 		typeValue.setText(pl.getType().toString());
@@ -42,10 +50,55 @@ public class Information extends Activity
 		else
 		rateValue.setText(String.valueOf(pl.getRate()));
 
-		final Button edit = (Button) findViewById(R.id.edit_button);
-			edit.setOnClickListener(new View.OnClickListener(){
+		edit = (Button) findViewById(R.id.edit_button);
+		save = (Button) findViewById(R.id.save_button);
+
+		edit.setOnClickListener(new View.OnClickListener(){
 			public void onClick(View v){
-				startActivity(new Intent(this, Edit.class));
+				nameValue.setVisibility(View.INVISIBLE);
+				nameValueEdit.setVisibility(View.VISIBLE);
+				nameValueEdit.setText(nameValue.getText());
+
+				typeValue.setVisibility(View.INVISIBLE);
+				typeValueEdit.setVisibility(View.VISIBLE);
+				typeValueEdit.setText(typeValue.getText());
+
+				paymentValue.setVisibility(View.INVISIBLE);
+				paymentValueEdit.setVisibility(View.VISIBLE);
+				paymentValueEdit.setText(paymentValue.getText());
+
+				limitValue.setVisibility(View.INVISIBLE);
+				limitValueEdit.setVisibility(View.VISIBLE);
+				limitValueEdit.setText(limitValue.getText());
+
+				rateValue.setVisibility(View.INVISIBLE);
+				rateValueEdit.setVisibility(View.VISIBLE);
+				rateValueEdit.setText(rateValue.getText());
+
+				save.setEnabled(true);
+				edit.setEnabled(false);
+			}
+		});
+
+		save.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View v){
+				nameValue.setVisibility(View.VISIBLE);
+				nameValueEdit.setVisibility(View.INVISIBLE);
+
+				typeValue.setVisibility(View.VISIBLE);
+				typeValueEdit.setVisibility(View.INVISIBLE);
+
+				paymentValue.setVisibility(View.VISIBLE);
+				paymentValueEdit.setVisibility(View.INVISIBLE);
+
+				limitValue.setVisibility(View.VISIBLE);
+				limitValueEdit.setVisibility(View.INVISIBLE);
+
+				rateValue.setVisibility(View.VISIBLE);
+				rateValueEdit.setVisibility(View.INVISIBLE);
+
+				edit.setEnabled(true);
+				save.setEnabled(false);
 			}
 		});
 	}
