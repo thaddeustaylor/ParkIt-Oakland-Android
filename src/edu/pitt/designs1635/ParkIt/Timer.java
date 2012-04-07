da6e336c7453f8095c3ee874d79539a763a74533
Thaddeus Taylor <tft3@pitt.edu>
2012-04-07 00:46:52 -0400

    Removed a conflict that happened at merge

--- src/edu/pitt/designs1635/ParkIt/Timer.java
+++ src/edu/pitt/designs1635/ParkIt/Timer.java
@@ -118,18 +118,4 @@ public class Timer extends Activity
 	    		
 	    	}
 	    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	switch (item.getItemId()) {
-    	case android.R.id.home:
-    		Intent intent = new Intent(this, ParkItActivity.class);
-    		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-    		startActivity(intent);
-    		return true;
-    	default:
-    		return super.onOptionsItemSelected(item);
-
-    	}
-    }
 }
