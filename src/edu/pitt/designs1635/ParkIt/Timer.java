010430ad9e142596eea3b4a85d0e451d07d17f06
Thaddeus Taylor <tft3@pitt.edu>
2012-04-24 00:15:25 -0400

    Added number wheels to the timer activity, added a cancel button, set the timer activity to only work in portrait mode

--- src/edu/pitt/designs1635/ParkIt/Timer.java
+++ src/edu/pitt/designs1635/ParkIt/Timer.java
@@ -1,5 +1,11 @@
 package edu.pitt.designs1635.ParkIt;
 
+import kankan.wheel.R;
+import kankan.wheel.widget.OnWheelChangedListener;
+import kankan.wheel.widget.OnWheelClickedListener;
+import kankan.wheel.widget.OnWheelScrollListener;
+import kankan.wheel.widget.WheelView;
+import kankan.wheel.widget.adapters.NumericWheelAdapter;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -7,17 +13,20 @@ import android.content.Intent;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
+import android.widget.Toast;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 public class Timer extends Activity
 {
-	private Spinner spinner, spinner2, spinner3;
+	//private Spinner spinner, spinner2, spinner3;
 	private int timerVal;
 
 	@Override
@@ -26,84 +35,97 @@ public class Timer extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timer);
 
-        spinner = (Spinner) findViewById(R.id.spinner1);
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-			this, R.array.intervals, android.R.layout.simple_spinner_item);
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner.setAdapter(adapter);
-		spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-			public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-				if(parent.getItemAtPosition(pos).toString().compareTo("120") == 0)
-					timerVal = 120;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("90") == 0)
-					timerVal =  90;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("60") == 0)
-					timerVal =  60;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("45") == 0)
-					timerVal =  45;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("30") == 0)
-					timerVal =  30;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("15") == 0)
-					timerVal =  15;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("5") == 0)
-					timerVal =  5;
-			}
-			public void onNothingSelected(AdapterView<?> parent) {
-			}
-		});
+		final WheelView meterHours = (WheelView) findViewById(R.id.meter_hour);
+		meterHours.setViewAdapter(new NumericWheelAdapter(this, 0, 23));
+	
+		
+		final WheelView meterMins = (WheelView) findViewById(R.id.meter_mins);
+		meterMins.setViewAdapter(new NumericWheelAdapter(this, 0, 59, "%02d"));
+		meterMins.setCyclic(true);
 
-		spinner2 = (Spinner) findViewById(R.id.spinner2);
-		ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
-			this, R.array.intervals, android.R.layout.simple_spinner_item);
-		adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner2.setAdapter(adapter2);
-		spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-			public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-				if(parent.getItemAtPosition(pos).toString().compareTo("120") == 0)
-					timerVal = 120;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("90") == 0)
-					timerVal =  90;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("60") == 0)
-					timerVal =  60;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("45") == 0)
-					timerVal =  45;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("30") == 0)
-					timerVal =  30;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("15") == 0)
-					timerVal =  15;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("5") == 0)
-					timerVal =  5;
+		
+		final WheelView alarmHours = (WheelView) findViewById(R.id.alarm_hour);
+		alarmHours.setViewAdapter(new NumericWheelAdapter(this, 0, 23));
+	
+		final WheelView alarmMins = (WheelView) findViewById(R.id.alarm_mins);
+		alarmMins.setViewAdapter(new NumericWheelAdapter(this, 0, 59, "%02d"));
+		alarmMins.setCyclic(true);
+		
+		/*
+		OnWheelClickedListener click = new OnWheelClickedListener() {
+            public void onItemClicked(WheelView wheel, int itemIndex) {
+                wheel.setCurrentItem(itemIndex, true);
+                Log.i("Timer Activity", wheel.getTag() + " / " + itemIndex);
+            }
+        };
+        */
+		
+		OnWheelScrollListener scrollListener = new OnWheelScrollListener() {
+			public void onScrollingStarted(WheelView wheel) {
+				
 			}
-			public void onNothingSelected(AdapterView<?> parent) {
+			
+			public void onScrollingFinished(WheelView wheel) {
+				
+				boolean incorrectAlarm = false;
+				
+				if(alarmHours.getCurrentItem() > meterHours.getCurrentItem())
+				{
+					alarmHours.setCurrentItem(meterHours.getCurrentItem(), true);
+					incorrectAlarm = true;
+				}
+				if(alarmHours.getCurrentItem() >= meterHours.getCurrentItem() &&
+				   alarmMins.getCurrentItem() > meterMins.getCurrentItem())
+				{
+					alarmMins.setCurrentItem(meterMins.getCurrentItem(), true);
+					incorrectAlarm = true;
+				}
+				
+				if (incorrectAlarm)
+					Toast.makeText(getApplicationContext(), "Alarm cannot be greater than the time on the meter.", Toast.LENGTH_SHORT).show();
+					
+				
+			}
+		};
+		
+		
+		
+        meterHours.setTag("Meter Hours");
+        meterMins.setTag("Meter Mins");
+        alarmHours.setTag("Alarm Hours");
+        alarmMins.setTag("Alarm Mins");
+		
+        //meterHours.addClickingListener(click);
+        //meterMins.addClickingListener(click);
+        //alarmHours.addClickingListener(click);
+        //alarmMins.addClickingListener(click);
+		
+        meterHours.addScrollingListener(scrollListener);
+        meterMins.addScrollingListener(scrollListener);
+        alarmHours.addScrollingListener(scrollListener);
+        alarmMins.addScrollingListener(scrollListener);
+        
+        
+		Button cancel = (Button) findViewById(R.id.alarmCancelButton);
+        Button start = (Button) findViewById(R.id.alarmStartButton);
+	
+        cancel.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View v){
+				finish();
 			}
 		});
-
-		spinner3 = (Spinner) findViewById(R.id.spinner3);
-		ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(
-			this, R.array.intervals, android.R.layout.simple_spinner_item);
-		adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner3.setAdapter(adapter3);
-		spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-			public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-				if(parent.getItemAtPosition(pos).toString().compareTo("120") == 0)
-					timerVal = 120;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("90") == 0)
-					timerVal =  90;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("60") == 0)
-					timerVal =  60;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("45") == 0)
-					timerVal =  45;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("30") == 0)
-					timerVal =  30;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("15") == 0)
-					timerVal =  15;
-				else if(parent.getItemAtPosition(pos).toString().compareTo("5") == 0)
-					timerVal =  5;
-			}
-			public void onNothingSelected(AdapterView<?> parent) {
+        
+        start.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View v){
+				Log.i("Timer Activity", "Meter: " + meterHours.getCurrentItem() + ":"
+						+ meterMins.getCurrentItem() + "  -- Alarm: " + 
+						alarmHours.getCurrentItem() + ":"
+						+ alarmMins.getCurrentItem());
 			}
 		});
+    
     }
+	
 	
 	   @Override
 	    public boolean onOptionsItemSelected(MenuItem item) {
