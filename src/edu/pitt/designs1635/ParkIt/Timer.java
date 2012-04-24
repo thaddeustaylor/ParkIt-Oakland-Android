73a77cff5649b1e05634f29372f05480d9f6c306
Thaddeus Taylor <tft3@pitt.edu>
2012-04-24 02:21:58 -0400

    Removed old imports

--- src/edu/pitt/designs1635/ParkIt/Timer.java
+++ src/edu/pitt/designs1635/ParkIt/Timer.java
@@ -1,35 +1,21 @@
 package edu.pitt.designs1635.ParkIt;
 
 import kankan.wheel.R;
-import kankan.wheel.widget.OnWheelChangedListener;
-import kankan.wheel.widget.OnWheelClickedListener;
 import kankan.wheel.widget.OnWheelScrollListener;
 import kankan.wheel.widget.WheelView;
 import kankan.wheel.widget.adapters.NumericWheelAdapter;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.ArrayAdapter;
-import android.widget.AdapterView;
-import android.widget.Toast;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockActivity;
 
 public class Timer extends SherlockActivity
 {
 	//private Spinner spinner, spinner2, spinner3;
-	private int timerVal;
+	//private int timerVal;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
